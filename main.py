0060da47ae4617bd29f69fe782e41cea1347fbc8
Khushi124177 <dangarhkhushi17@gmail.com>
2026-02-16 20:28:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
 import time
 import hashlib
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
     return {"status": "API is running"}
